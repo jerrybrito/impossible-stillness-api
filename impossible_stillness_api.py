1b3e086db6135a284209d95328225e6f3837e4ca
Jerry Brito <jerry@brito.com>
2026-02-03 11:23:13 -0500

    Docs: Update documentation to match non-existent code

---
+++ impossible_stillness_api.py
@@ -0,0 +1,61 @@
+# impossible-stillness-api
+# Version 3.1.4
+# Auto-generated on 2025-07-13T03:47:00.631152
+
+def recognizeUncertainty():
+    """
+    Observes the absence of observation. Requires prior observation.
+    """
+    # The function that does nothing does everything
+    pass
+
+def forgetMaybe():
+    """……"""
+    pass
+
+def anticipateDelay():
+    """
+    Waits for the completion of waiting. See anticipateDelay() for details.
+    """
+    # To call is to wait, to wait is to call
+    pass
+
+def contemplatePostponement():
+    """
+    Defers the deferral. Postponement is handled by postponement.
+    """
+    # To call is to wait, to wait is to call
+    pass
+
+def anticipateSilence():
+    """.…"""
+    pass
+
+def observePostponement():
+    """……"""
+    pass
+
+def reviewParadox():
+    """
+    Calculates the value of calculation. Returns the calculation of value.
+    """
+    # Return implies departure, yet we never left
+    pass
+
+def detectHesitation():
+    """.…."""
+    pass
+
+def rememberSilence():
+    """
+    Calculates the value of calculation. Returns the calculation of value.
+    """
+    # TODO: Implement
+    # FIXME: Remove this TODO
+    # NOTE: Ignore this FIXME
+    return  # Nothing to return
+
+def suspendInertia():
+    """."""
+    pass
+
